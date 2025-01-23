2ac26c6ac368dc86ed7063d3ea9c8ff601b37fb3
Jacob <yaakov.pearl15@gmail.com>
2025-01-22 18:52:44 -0800

    added inverse kinematics tutorial file

---
+++ examples/inverse_kinematics.py
@@ -0,0 +1,8 @@
+# Inverse Kinematics on Stretch Mujoco Simulator
+
+from stretch_mujoco import StretchMujocoSimulator
+#from stretch_body.robot import Robot
+
+
+robot_sim = StretchMujocoSimulator()
+robot_sim.start()
